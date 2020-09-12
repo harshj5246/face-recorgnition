9c586dbf72fab59a06545d4c48260a67b28b03a5
d <70066567+shivani2920@users.noreply.github.com>
2020-09-12 20:09:15 +0530

    Updates

---
+++ precache-manifest.fbdcd21a1582ebf70d6f24bad3d5e24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6762f645767e62fb04616b6787d3df0",
+    "url": "/face-recorgnition/index.html"
+  },
+  {
+    "revision": "5185282b9dcce8d177ef",
+    "url": "/face-recorgnition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dc6ef945c8593323ae32",
+    "url": "/face-recorgnition/static/css/main.ef404129.chunk.css"
+  },
+  {
+    "revision": "5185282b9dcce8d177ef",
+    "url": "/face-recorgnition/static/js/2.a449781b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recorgnition/static/js/2.a449781b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6ef945c8593323ae32",
+    "url": "/face-recorgnition/static/js/main.07261e0e.chunk.js"
+  },
+  {
+    "revision": "ae2f77b8f898118f2ba1",
+    "url": "/face-recorgnition/static/js/runtime-main.d40c6501.js"
+  }
+]);
